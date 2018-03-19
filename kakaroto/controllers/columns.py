4168a76a57dcf6dcbd4072d51a8eaa354468f77c
glimaux <o.gabrielima@gmail.com>
2018-03-18 22:59:41 -0300

    add endpoint to setup columns

---
+++ kakaroto/controllers/columns.py
@@ -0,0 +1,35 @@
+from sanic.views import HTTPMethodView
+from sanic.response import json
+from decorators.authorization import authorization
+
+from database.models import Board_Column
+from database.session import Session
+from extensions.json import extended_dumps
+
+
+class Collection(HTTPMethodView):
+    decorators = [authorization]
+
+    # TODO: Adicionar validação como decorator
+    async def post(self, request, project_id):
+        session = Session()
+        try:
+
+            item = Board_Column().fromdict(request.json, allow_pk=True)
+            item.project_id = project_id
+
+            session.add(item)
+            session.commit()
+
+            # TODO: controle dos contratos das repostas de forma centralizada -> return SuccessReponse(item.asdict)
+            return json(
+                {
+                    'success': True,
+                    'data': item.asdict()
+                },
+                status=201,
+                dumps=extended_dumps
+            )
+
+        finally:
+            session.close()
--- kakaroto/main.py
+++ kakaroto/main.py
@@ -1,13 +1,14 @@
 from sanic import Sanic
 
 from config import APP_NAME
-from controllers import cards, issues, projects
+from controllers import cards, issues, projects, columns
 
 app = Sanic(APP_NAME)
 
 app.add_route(cards.Webhook.as_view(), '/cards/hooks', methods=['POST'], version=1)
 app.add_route(issues.Webhook.as_view(), '/issues/hooks', methods=['POST'], version=1)
 app.add_route(projects.Collection.as_view(), '/projects', methods=['POST'], version=1)
+app.add_route(columns.Collection.as_view(), '/projects/<project_id:string>/columns', methods=['POST'], version=1)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3443)
